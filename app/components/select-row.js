3918d6d8f57d11837e43c1b557d50c5364dc4247
Jessica Yu <jessyu2010@gmail.com>
2020-12-10 23:26:00 -0800

    Test click fn added

---
+++ app/components/select-row.js
@@ -0,0 +1,9 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class SelectRowComponent extends Component {
+  @action
+  showMessage(gene) {
+    alert(`The gene is ${gene}!`);
+  }
+}
