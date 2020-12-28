58b7192d636be2117df487c3c3eeb6dfcd3d1696
Jessica Yu <jessyu2010@gmail.com>
2020-12-28 00:00:52 -0800

    button clickability implemented

---
+++ tests/integration/components/slider-button-test.js
@@ -0,0 +1,26 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | slider-button', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<SliderButton />`);
+
+    assert.equal(this.element.textContent.trim(), '');
+
+    // Template block usage:
+    await render(hbs`
+      <SliderButton>
+        template block text
+      </SliderButton>
+    `);
+
+    assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+});
