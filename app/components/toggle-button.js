b0c3f858b9cda07c31e1b742b562fbed2212159f
Jessica Yu <jessyu2010@gmail.com>
2020-12-27 23:14:14 -0800

    Toggle buttons

--- app/components/toggle-button.js
+++ app/components/toggle-button.js
@@ -1,9 +1,10 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class ToggleButtonComponent extends Component {
-  @action
-  toggleButton() {
-    console.log(`Button toggled!`);
+  @tracked isSelected = false;
+  @action toggleButton() {
+    this.isSelected = !this.isSelected;
   }
 }
